3bb9e12ad8880f16c37afb76c2a02fd5b0bea91f
Andy Braga <bragovo@gmail.com>
2023-11-10 16:18:36 +0300

    Switch to new model (#3)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 @app.post("/summarizer")
 async def root(request: Request):
   json = await request.json()
-  result = summarizer(json["text"], truncation=True)
+  text = "summarize: " + json["text"]
+  result = summarizer(text, truncation = True, num_beams = 3, max_new_tokens = 200, repetition_penalty = 10.0)
 
   return { "text": result[0]["summary_text"] }
