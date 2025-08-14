d010dcd7d58983e3bd4f2b876bdcf41c76a0dc55
Andy Braga <bragovo@gmail.com>
2025-08-14 17:21:23 +0300

    Bump prost & tonic (#6)
    * Bump prost & tonic
    
    * To main

--- lib/api/build.rs
+++ lib/api/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(false)
         .compile_protos(&["src/notify.proto"], &["src"])
         .unwrap();
